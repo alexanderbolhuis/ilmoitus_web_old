6298efa9b9f719da9590d21408a45d7121afbd76
RobinBlok <robinrobin_1@hotmail.com>
2014-04-06 22:48:56 +0200

    WEB01 Declaraties tonen
    Non-angular code removed.
    Fetches declarations for current employee, with page filler as backup.

--- Ilmoitus/javascript/controllers.js
+++ Ilmoitus/javascript/controllers.js
@@ -28,15 +28,39 @@ ilmoitusApp.controller('templateController', function($scope, $state) {
     }
 });
 
-ilmoitusApp.controller('declarationsController', function($scope, $state) {
-	$scope.navBtnSelect("declarationsBtn");
-	SetTableSelectable("declarationTable");
-	
-	//Metod to open the declaration Details page
-	$scope.openDeclarationDetails = function(declarationId){
-		console.log("declaratie ID: " + getTableSelectedItem("declarationTable"));
-		$state.go('template.declarationDetails', {declarationId: declarationId});
+ilmoitusApp.controller('declarationsController', function($scope, $state, $http) {
+	$http.get('/declarations/employee').then(function(res){
+		$scope.declarationList = res.data;
+	}, function(err) { 
+		//Data not fetched, use backup
+		//TODO: Delete this function and show some error
+		$scope.declarationList = [
+			{ 'id':'1', 'created_at':'06-03-2014', 'declaraionlines':[''], 'totalprice':'100,50', 'state':'Goedgekeurd', 'comment':'Komt op loonstrook voor: 31-4-2012' },
+			{ 'id':'2', 'created_at':'07-03-2014', 'declaraionlines':['', '', ''], 'totalprice':'50', 'state':'In behandeling', 'comment':'' },
+			{ 'id':'3', 'created_at':'08-03-2014', 'declaraionlines':['', ''], 'totalprice':'85,65', 'state':'Openstaand', 'comment':'' }
+		];
+	});
+		
+	//Select declaration
+	$scope.selectDeclaration = function(declaration){
+		$scope.currentdeclaration = declaration;
 	}
+	
+	//Doubleclick declaration
+	$scope.openDeclarationDetails = function(declarationid){
+ 		$state.go('template.declarationDetails', {declarationId: declarationid});
+  	}
+	
+	//Open declaration button
+	$scope.openDeclarationDetailsBtn = function(declarationid){
+ 		$state.go('template.declarationDetails', {declarationId: $scope.currentdeclaration.id});
+  	}
+	
+	//Delete declaration button
+	$scope.deleteDeclarationDetailsBtn = function(declarationid){
+ 		//TODO: bind DELETE handler
+		alert('Consider declaration "'+$scope.currentdeclaration.id+'" deleted.');
+  	}
 });
 
 ilmoitusApp.controller('newDeclarationController', function($scope) {
